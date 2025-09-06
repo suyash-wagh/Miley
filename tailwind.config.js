d28aad90e1a626d09fd3ceadb982bba95bd6049c
Suyash Wagh <suyashwagh22@gmail.com>
2025-09-06 11:37:15 +0530

    Fixed issue with the project structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,12 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './styles/**/*.{js,ts,jsx,tsx,mdx,css}',
+    './app/**/*.{js,ts,jsx,tsx,mdx,css}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
